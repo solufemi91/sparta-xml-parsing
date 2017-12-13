17de80ea2f799d0edd13b9937e06af99a3edff98
solufemi91 <stevenoo@spartaglobal.com>
2017-12-13 22:03:17 +0000

    2nd test half working

--- starter_code_xml_lab/spec/xml_menu_spec.rb
+++ starter_code_xml_lab/spec/xml_menu_spec.rb
@@ -7,13 +7,18 @@ describe "XML menu" do
   end
 
   it "no price should be more than £10" do
-     @xml_menu.get_price.each do |price|
+     @xml_menu.get_all_prices_as_floats.each do |price|
        expect(price).to be < 10
      end
   end
 
   it "should have no item with calories over 1000 except for the full breakfast" do
     # Consider children element methods - Remember to step through the data and print out to the command line if you need it
+
+    #  go through each food tag, check that its name is not full breakfast and then check its calorie tag is less than 1000
+    @xml_menu.get_all_calorie_tags.each do |calories|
+      expect(calories).to be < 1000
+    end
   end
 
   it "should have all waffle dishes stating you get two waffles" do
--- starter_code_xml_lab/xml_parsing.rb
+++ starter_code_xml_lab/xml_parsing.rb
@@ -8,10 +8,6 @@ class GuiseppesMenu
     @menu = Nokogiri::XML(File.open('./xml_menu.xml'))
   end
 
-  def get_price
-    @menu.search('price')
-  end
-
   def get_all_prices_as_floats
     price_array = []
     @menu.xpath('//price').each do |price|
@@ -19,12 +15,59 @@ class GuiseppesMenu
     end
 
     price_array
+  end
+
+  def get_all_calorie_tags
+     #get all the calorie tags, except the one that has a name sibling of breakfast
+    #how do i select a tag that has a specific sibling
+    calories_array = []
+    @menu.xpath('//calories').each do |calories|
+      calories_array << calories.text.to_i
+    end
+
+    calories_array
+
+    # @menu.xpath("/breakfast_menu/food[not(name='Full Breakfast')]")
+
 
   end
+
+
+
+
 
   # Place your methods here
 
 end
 
 x = GuiseppesMenu.new
-puts x.get_price.text
+puts x.get_all_calorie_tags
+# puts x.get_all_foods_except_breakfast
+#
+# class GuiseppesMenu
+#   attr_accessor :menu
+#
+#   def initialize
+#     @menu = Nokogiri::XML(File.open('./xml_menu.xml'))
+#   end
+#
+#   def get_menu_names
+#     @menu.search('name')
+#   end
+#
+#   def xpath_get_names
+#     @menu.xpath('//calories')
+#   end
+#
+#   def xpath_get_nameOfFoods
+#     @menu.xpath('//name')
+#   end
+#
+#   def xpath_get_nameOfFoodsNoTags
+#     @menu.xpath('//name').text
+#   end
+#
+#
+#
+#
+# end
